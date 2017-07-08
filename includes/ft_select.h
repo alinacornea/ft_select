aed2d5d287c7593719706ea76cd371668ea1a433
Alina Cornea <alinacornea@MacBook-Pro-2.local>
2017-07-07 19:24:30 -0700

    ad

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -17,14 +17,30 @@
 # include <stdlib.h>
 # include <curses.h>
 # include <termcap.h>
-# include "/usr/include/term.h"
+# include <term.h>
 
 # define TERM_BUF 2048
+# define KEY_ASCII(c)	(c)
+# define KEY_ESC		27
+# define KEY_BACK		127
+# define KEY_DELETE		2117294875
+# define KEY_RETURN		10
+# define KEY_END		4607771
+# define KEY_UP			4283163
+# define KEY_LEFT		4479771
+# define KEY_RIGHT		4414235
+# define KEY_DOWN		4348699
+# define KEY_CTRL_A		1
+# define KEY_CTRL_E		5
 
 typedef struct s_select
 {
-    char *term;
+    struct termios term;
+    char          *name;
+    int col;
+    int row;
     
+
 }              t_select;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -12,28 +12,89 @@
 
 #include "ft_select.h"
 
+// int main(int argc, char **argv)
+// {
+// 	t_select tm;
+// 	char buffer[TERM_BUF];
+//
+// 	if(argc > 2)
+// 		printf("Usage: %s\n", argv[0]);
+// 	//init_term();
+// 	if (argc == 2)
+// 		tm.term = argv[1];
+// 	else
+// 		tm.term = getenv("TERM");
+// 	if (tm.term == NULL)
+// 		printf("Error: $TERM is not defined\n");
+// 	/*read the termcap entry */
+// 	if (tgetent(buffer, tm.term) != -1)
+// 		printf("Error: No termcap entry for %s\n", tm.term);
+// 	printf("TERM = %s\n\n", tm.term);
+// 	if (tgetflag("am") == 1)
+// 		printf("end of line wraps to next line (am) \n");
+// 	if (tgetflag("bs") == 1)
+// 		printf("end of line wraps to next line (bs) \n");
+// 	printf("number of colums (co) = %d\n", tgetnum("co"));
+// 	printf("number of colums (li) = %d\n", tgetnum("li"));
+// }
+
+static void sig_cont(void)
+{
+	t_select *tm;
+
+	tm = NULL;
+	tm = tm_stock(tm, 1);
+	tm->term.c
+}
+void get_signal(int i)
+{
+	if (i == SIGCONT)
+		sig_cont();
+	else if (i == SIGTSTP)
+		sig_stop();
+	else if (i == SIGWINCH)
+		sig_size();
+	else
+		sig_end();
+}
+
+void tm_signal(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 32)
+	{
+		signal(i, get_signal)
+		i++;
+	}
+}
+int tm_init(t_select *tm)
+{
+	struct winsize win;
+
+	if ((t_getent(NULL, getenv("TERM")) < 1) ||
+	(tcgetattr(0, &(tm->term)) == -1))
+		return (0);
+	// tm->term.c_lflag
+	tm->term.c_cc[VMIN] = 1;
+	tm->term.c_cc[VTIME] = 0;
+	tm->col = win.ws_col;
+	tm->row = win.ws_row;
+	if (tcsetattr(0, 0, &(tm->term)) == -1)
+		return (0);
+	tputs(tgetstr("ti", NULL), 1, ft_putchar);
+	tputs(tgetstr("vi", NULL), 1, ft_putchar);
+	return (1);
+}
+
 int main(int argc, char **argv)
 {
 	t_select tm;
-	char buffer[TERM_BUF];
 
-	if(argc > 2)
-		printf("Usage: %s\n", argv[0]);
-	//init_term();
-	if (argc == 2)
-		tm.term = argv[1];
-	else 
-		tm.term = getenv("TERM");
-	if (tm.term == NULL)
-		printf("Error: $TERM is not defined\n");
-	/*read the termcap entry */
-	if (tgetent(buffer, tm.term) != -1)
-		printf("Error: No termcap entry for %s\n", tm.term);
-	printf("TERM = %s\n\n", tm.term);
-	if (tgetflag("am") == 1)
-		printf("end of line wraps to next line (am) \n");
-	if (tgetflag("bs") == 1)
-		printf("end of line wraps to next line (bs) \n");
-	printf("number of colums (co) = %d\n", tgetnum("co"));
-	printf("number of colums (li) = %d\n", tgetnum("li"));
+	tm_signal();
+	tm_init(&tm);
+	(argc >= 2) ? tm_select(argv, &tm);
+	(!tm_end(&tm)) ? (-1) : (0);
+	return (0);
 }
